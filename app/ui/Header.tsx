c4239f81e660d916ba977e94ff1b243a073fe836
Pawel Tomczak <paweltomczak90@gmail.com>
2024-04-04 22:49:32 +0200

    Adding fonts

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -6,6 +6,7 @@ import {
   MagnifyingGlassIcon,
   XMarkIcon,
 } from '@heroicons/react/24/solid';
+import { kanit } from './fonts';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -15,7 +16,6 @@ const Header = () => {
     <header className='bg-white my-3 pb-3'>
       <div className='max-w-6xl mx-auto px-4'>
         <div className='flex justify-between items-center py-3'>
-          {/* Toggle Menu Icon for Mobile */}
           <div className='lg:hidden flex-1'>
             <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
               {isMenuOpen ? (
@@ -26,15 +26,15 @@ const Header = () => {
             </button>
           </div>
 
-          {/* Brand Name - Centered on mobile and Left on PC */}
           <div className='flex-1 lg:flex-none'>
             <div className='text-center lg:text-left'>
-              <span className='font-brand text-3xl font-medium'>Verbitus</span>
+              <span className={`${kanit.className} text-3xl font-medium`}>
+                Verbitus
+              </span>
             </div>
           </div>
 
           <div className='hidden lg:flex flex-grow justify-center items-center space-x-4'>
-            {/* Iterate over categories. This could also come from a dynamic list */}
             {['Category 1', 'Category 2', 'Category 3'].map((category) => (
               <a
                 key={category}
@@ -51,13 +51,11 @@ const Header = () => {
             ))}
           </div>
 
-          {/* Search Icon - Right-aligned on all sizes */}
           <div className='flex-1 lg:flex-none flex justify-end'>
             <MagnifyingGlassIcon className='h-6 w-6' />
           </div>
         </div>
 
-        {/* Mobile Menu - show when menu is open */}
         <div className={`lg:hidden ${isMenuOpen ? 'block' : 'hidden'} px-4`}>
           <a href='#' className='block px-3 py-2 hover:bg-gray-100'>
             Category 1
---
+++ app/ui/fonts.ts
@@ -0,0 +1,22 @@
+import { Kanit } from 'next/font/google';
+import { DM_Sans } from 'next/font/google';
+
+export const kanit = Kanit({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+export const dm_sans = DM_Sans({
+  subsets: ['latin'],
+  weight: [
+    '100',
+    '200',
+    '300',
+    '400',
+    '500',
+    '600',
+    '700',
+    '800',
+    '900',
+    '1000',
+  ],
+});
