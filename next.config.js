c99c81dcae2b2ea1fa68cf1a8849b2aabe883052
Pawel Tomczak <paweltomczak90@gmail.com>
2024-04-20 02:47:14 +0200

    Images ompimalization

--- next.config.js
+++ next.config.js
@@ -7,7 +7,31 @@ const nextConfig = {
         hostname: 'firebasestorage.googleapis.com',
       },
     ],
-    minimumCacheTTL: 60,
+    minimumCacheTTL: 31536000, // Increasing TTL to one year
+  },
+  async headers() {
+    return [
+      {
+        // Targeting optimized image requests
+        source: '/_next/image',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        // Apply caching headers for all images
+        source: '/(.*)\\.(jpg|jpeg|png|gif|webp|ico|svg)$',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
   },
 };
 
