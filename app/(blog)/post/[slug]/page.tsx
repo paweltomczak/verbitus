e0698b1c719d4f6b21dbc2b4f532aa034057f250
Pawel Tomczak <paweltomczak90@gmail.com>
2024-04-22 13:38:46 +0200

    Fix search on post flow

--- app/(blog)/post/[slug]/page.tsx
+++ app/(blog)/post/[slug]/page.tsx
@@ -1,13 +1,29 @@
 import { Spinner } from '@/app/ui/common/loaders';
+import { HomePagePosts } from '@/app/ui/posts/HomePagePosts';
 import { PostDetails } from '@/app/ui/posts/PostDetails';
 import { Suspense } from 'react';
 
-export default async function Page({ params }: { params: { slug: string } }) {
+interface PageProps {
+  params: {
+    slug: string;
+  };
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}
+
+export default function Page({ params, searchParams }: PageProps) {
+  const query = searchParams?.query || '';
   const { slug } = params;
 
-  return (
+  return query.length === 0 ? (
     <Suspense fallback={<Spinner />}>
       <PostDetails slug={slug} />
+    </Suspense>
+  ) : (
+    <Suspense fallback={<Spinner />}>
+      <HomePagePosts query={query} />
     </Suspense>
   );
 }
