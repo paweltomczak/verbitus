8bc4e0b2b4881dd4a8b69ec617632dd15ea097d0
Pawel Tomczak <paweltomczak90@gmail.com>
2024-05-16 23:18:54 +0200

    Updating maxDuration

--- app/(admin)/dashboard/posts/create/page.tsx
+++ app/(admin)/dashboard/posts/create/page.tsx
@@ -2,6 +2,8 @@ import { CreatePost } from '@/app/ui/dashboard/posts/CreatePost';
 import { Spinner } from '@/app/ui/common/loaders';
 import { Suspense } from 'react';
 
+export const maxDuration = 20;
+
 export default async function Page() {
   return (
     <Suspense fallback={<Spinner />}>
