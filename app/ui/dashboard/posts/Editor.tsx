48412ffd59685f47cd8b0a0df5dc08f42df60fb8
Pawel Tomczak <paweltomczak90@gmail.com>
2024-05-18 23:16:49 +0200

    Adding code-block and video format to Editor

--- app/ui/dashboard/posts/Editor.tsx
+++ app/ui/dashboard/posts/Editor.tsx
@@ -21,14 +21,14 @@ export default function Editor({
   const modules = {
     toolbar: [
       [{ header: [1, 2, 3, false] }],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote', 'code-block'],
       [
         { list: 'ordered' },
         { list: 'bullet' },
         { indent: '-1' },
         { indent: '+1' },
       ],
-      ['link', 'image'],
+      ['link', 'image', 'video'],
       ['clean'],
     ],
   };
